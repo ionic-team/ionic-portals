4084eb176d0b233f940731bbdf2edffbd61d2043
Carl Poole <mail@carlpoole.com>
2021-09-07 13:47:54 -0500

    feat(android): update the portals registration URL (#25)

--- android/IonicPortals/src/main/kotlin/io/ionic/portals/PortalManager.kt
+++ android/IonicPortals/src/main/kotlin/io/ionic/portals/PortalManager.kt
@@ -11,14 +11,14 @@ object PortalManager {
     @JvmStatic private var registered: Boolean = true
 
     /**
-     * Adds a Portal object given the name of the portal
-     * @param name The Portal name
+     * Adds a Portal to the set of Portals
+     * @param portal The Portal to add
      */
     @JvmStatic fun addPortal(portal: Portal) {
         portals[portal.name] = portal
 
         if (!registered) {
-            Log.e("Portals", "Don't forget to register your copy of portals! Register at: ionic.io/portals")
+            Log.e("Portals", "Don't forget to register your copy of portals! Register at: ionic.io/register-portals")
         }
     }
 
