f10b373d5bafdf6b7173da70034f16612d75d5ab
Carl Poole <carl@ionic.io>
2021-09-07 16:43:55 -0500

    initial publishing script for android

---
+++ scripts/publish-android.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+DIR=..
+LOG_OUTPUT=./tmp/portals-android.txt
+PORTALS_VERSION=`grep '"version": ' $DIR/plugin/package.json | awk '{print $2}' | tr -d '",'`
+echo Attempting to build and publish Portals native library with version $PORTALS_VERSION
+
+# Get the latest version of Capacitor
+CAPACITOR_PACKAGE_JSON="https://raw.githubusercontent.com/ionic-team/capacitor/main/android/package.json"
+CAPACITOR_VERSION=$(curl -s $CAPACITOR_PACKAGE_JSON | awk -F\" '/"version":/ {print $4}')
+
+# Don't continue if there was a problem getting the latest version of Capacitor
+if [[ $CAPACITOR_VERSION ]]; then
+    printf %"s\n\n" "Attempting to publish new Portals with dependency on Capacitor Version $CAPACITOR_VERSION"
+else
+    printf %"s\n\n" "Error resolving latest Capacitor version from $CAPACITOR_PACKAGE_JSON"
+    exit 1
+fi
+
+# Get latest com.capacitorjs:core XML version info
+CAPACITOR_PUBLISHED_URL="https://repo1.maven.org/maven2/com/capacitorjs/core/maven-metadata.xml"
+CAPACITOR_PUBLISHED_DATA=$(curl -s $CAPACITOR_PUBLISHED_URL)
+CAPACITOR_PUBLISHED_VERSION="$(perl -ne 'print and last if s/.*<latest>(.*)<\/latest>.*/\1/;' <<< $CAPACITOR_PUBLISHED_DATA)"
+
+# Check if we need to publish a new native version of the Capacitor Android library
+if [[ "$CAPACITOR_VERSION" != "$CAPACITOR_PUBLISHED_VERSION" ]]; then
+    printf %"s\n" "Publish Capacitor Core first! The latest published Android library version $CAPACITOR_PUBLISHED_VERSION in MavenCentral is outdated. There is an unpublished version $CAPACITOR_VERSION in ionic-team/capacitor."
+    exit 1
+else
+    # Capacitor version in MavenCentral is up to date, continue publishing the native Portals library
+    printf %"s\n\n" "Latest native Capacitor Android library is version $CAPACITOR_PUBLISHED_VERSION and is up to date, continuing with Portals publishing..."
+
+
+
+
+
+fi
