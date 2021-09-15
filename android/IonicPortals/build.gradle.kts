b5cf3ab8f52fc964a7f27489e801f4c83ae70367
Carl Poole <mail@carlpoole.com>
2021-09-15 09:39:20 -0500

    chore(android): updated Capacitor core dependency to 3.2.2 (#37)

--- android/IonicPortals/build.gradle.kts
+++ android/IonicPortals/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 
 dependencies {
     implementation(kotlin("reflect"))
-    implementation("com.capacitorjs:core:3.2.1")
+    implementation("com.capacitorjs:core:3.2.2")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.21")
     implementation( "androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
