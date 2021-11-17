6ecc70be4ea66bcc0a9fd34e6af0815cd379183c
Nathan Walker <walkerrunpdx@gmail.com>
2021-11-16 18:29:50 -0800

    chore: cleanup xcode build

--- ios/IonicPortals/IonicPortals/PortalManager.swift
+++ ios/IonicPortals/IonicPortals/PortalManager.swift
@@ -20,10 +20,6 @@ public class PortalManager: NSObject {
     private static var registeredError = false
     private static var unregisteredMessageShown = false
 
-    // Initialization
-
-    private init() {}
-
     /**
      * Returns the number of `Portal` objects in the `PortalManager`.
      * - returns: The count of `Portal` objects.
