180ac8d9acf7202d27c206baf9917863fd7364cc
Thomas Vidas <hello@thomasvidas.com>
2022-02-18 11:41:11 -0500

    fix(ios): Carthage test for IonicPortals 0.5.0

---
+++ ios/Frameworks/IonicLiveUpdates.xcframework/ios-arm64_x86_64-simulator/IonicLiveUpdates.framework/Headers/LiveUpdatesSDK.h
@@ -0,0 +1,18 @@
+//
+//  LiveUpdatesSDK.h
+//  LiveUpdatesSDK
+//
+//  Created by Thomas Vidas on 11/29/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LiveUpdatesSDK.
+FOUNDATION_EXPORT double LiveUpdatesSDKVersionNumber;
+
+//! Project version string for LiveUpdatesSDK.
+FOUNDATION_EXPORT const unsigned char LiveUpdatesSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiveUpdatesSDK/PublicHeader.h>
+
+
---
+++ ios/IonicPortals.xcframework/ios-arm64_x86_64-simulator/IonicPortals.framework/Headers/IonicPortals.h
@@ -0,0 +1,18 @@
+//
+//  IonicPortals.h
+//  IonicPortals
+//
+//  Created by Thomas Vidas on 7/13/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IonicPortals.
+FOUNDATION_EXPORT double IonicPortalsVersionNumber;
+
+//! Project version string for IonicPortals.
+FOUNDATION_EXPORT const unsigned char IonicPortalsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IonicPortals/PublicHeader.h>
+
+
