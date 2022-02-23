71975b5861ee33a3a9bab8a936ca8f31d82f136b
Dan Giralté (Ionic) <giralte@ionic.com>
2022-02-23 12:24:55 -0600

    Remove iOS and Android everything, remove references from files

--- ios/IonicPortals/IonicPortals/PortalsPlugin.m
+++
@@ -1,7 +0,0 @@
-#import <Capacitor/Capacitor.h>
-
-CAP_PLUGIN(PortalsPlugin, "Portals",
-  CAP_PLUGIN_METHOD(publishNative, CAPPluginReturnPromise);
-  CAP_PLUGIN_METHOD(subscribeNative, CAPPluginReturnCallback);
-  CAP_PLUGIN_METHOD(unsubscribeNative, CAPPluginReturnPromise);
-)
--- ios/IonicPortals/IonicPortals/UnregisteredView.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-import UIKit
-
-public class UnregisteredView: UIView {
-    
-
-}
