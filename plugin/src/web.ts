b6ac27b02892fb954586b9deeabf6a131db161dc
Carl Poole <carl@ionic.io>
2024-04-24 15:02:33 -0500

    feat: Capacitor 6 upgrade (#273)

--- plugin/src/web.ts
+++
@@ -1,8 +0,0 @@
-import { WebPlugin } from '@capacitor/core';
-
-import type { PortalMessage, PortalsPlugin } from './definitions';
-
-export class PortalsWeb extends WebPlugin implements PortalsPlugin {
-  // eslint-disable-next-line
-  async publishNative(_message: PortalMessage): Promise<void> {}
-}
